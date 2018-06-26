4ba614343039ae2b7ea857b69df33ef30dee84f1
analyst <analyst@Analysis>
2018-06-26 16:55:03 -0500

    updates

--- Readme.md
+++ Readme.md
@@ -4,7 +4,16 @@ Small script that exports IOCs from your AlienVault OTX subscription to csv and 
 
 Edit the api_key in the script with the api key from your OTX account (https://otx.alienvault.com/api) before running.
 
-## Syntax
+## Dependencies
+Unidecode is the only non-standard dependency at this time:
 
-```python otx_feed.py [output.csv]```
+```pip install unidecode```
+
+## Usage
+
+### Linux:
+`python otx_feed.py [output.csv]`
+
+### Windows:
+```C:\Python27\python.exe .\otx_feed.py [output.csv]```
 
--- otx_feed.py
+++ otx_feed.py
@@ -4,58 +4,81 @@ import json
 import re
 import datetime
 import sys
+import unidecode
 api_key=""
-
-if len(api_key.strip())<1:
-	print("No api key specified.")
-	sys.exit(1)
-now=datetime.datetime.utcnow()
-yesterday=datetime.datetime(now.year,now.month,now.day-1,now.hour,now.minute,now.second,now.microsecond).isoformat()
-
-response=requests.get("https://otx.alienvault.com/api/v1/indicators/export?limit=5000&modified_since="+yesterday.strip(),headers={"X-OTX-API-KEY":api_key})
-#print(response.text)
-jdata=json.loads(response.text)
-ioclist={"domain":[],"ipv4":[],"md5":[],"sha256":[]}
-if "results" in jdata:
-	output=""
-	for r in jdata["results"]:
-		if "indicator" in r:
-			if "type" in r:
-				description=""
-				if "description" in r:
-					description=r["description"]
-				if r["type"] in ["domain","hostname"]:
-					ioclist["domain"].append([r["indicator"].strip(),description])
-				elif r["type"].lower().strip() == "filehash-md5":
-					ioclist["md5"].append([r["indicator"],description])
-				elif r["type"].lower().strip() == "filehash-sha256":
-					ioclist["sha256"].append([r["indicator"],description])
-				elif r["type"].lower().strip() == "ipv4":
-					ioclist["ipv4"].append([r["indicator"],description])
-				elif r["type"].lower().strip() == "url":
-					m=re.match(r"https?://([\w\d\.\-]*)[\:/]+.*",r["indicator"])
-					if not None is m and not None is m.group(1):
-						m2=re.match("\d+\.\d+\.\d+\.\d+",m.group(1))
-						if not None is m2:
-							ioclist["ipv4"].append([m.group(1),description])
-						else:
-							ioclist["domain"].append([m.group(1),description])
-filename="ioclist.csv"
-if len(sys.argv)>1 and len(sys.argv[1])>1:
-	filename=sys.argv[1]
-with open(filename,"w+") as iocfile:				
-	print("IOC Type\tIndicator\tDescription")
-	iocfile.write("IOC Type,Indicator,Description\n")
-	for k in ioclist:
-		#print(("*"*50)+k+("*"*50))
-		for e in ioclist[k]:
-			desc="\n"
-			odesc=""
-			if len(e[1].strip())<1:
-				desc=",\n"
-				odesc=" <no description>"
-			else:
-				desc=e[1]
-			print k+"\t"+e[0]+"\t"+odesc
-			iocfile.write(k+","+e[0]+","+desc)
-print("ioc list written to "+filename)
+if __name__ == "__main__":
+	reload(sys)
+	sys.setdefaultencoding('UTF8')
+	if len(api_key.strip())<1:
+		print("No api key specified.")
+		sys.exit(1)
+	now=datetime.datetime.utcnow()
+	yesterday=datetime.datetime(now.year,now.month,now.day-1,now.hour,now.minute,now.second,now.microsecond).isoformat()
+	
+	response=requests.get("https://otx.alienvault.com/api/v1/pulses/subscribed?limit=5000&modified_since="+yesterday.strip(),headers={"X-OTX-API-KEY":api_key})
+	#print(response.text)
+	jdata=json.loads(response.text)
+	ioclist={"domain":[],"ipv4":[],"md5":[],"sha256":[]}
+	for p in jdata["results"]:
+		for res in p:
+			
+			#print str(res)+">>>>>>>>>>>>>"+str(p[res])
+			name=""
+			if "name" in p:
+				name=unidecode.unidecode(str(p["name"]))[:128].replace(",",";").replace("\n",";")
+			description=""
+			if "description" in p:
+				description=unidecode.unidecode(str(p["description"]))[:512].replace(",",";").replace("\n",";")
+			author=""
+			
+			if "author" in p:
+				author=unidecode.unidecode(str(p["author"]))[:64].replace(",",";").replace("\n",";")
+	
+			references=""
+			if "references" in p:
+				if not type(p["references"]) is list:
+					references=unidecode.unidecode(str(p["references"]))[:64].replace(",",";").replace("\n",";")
+				else:
+					refs=';'.join(p["references"][:5]).strip(";")
+					references=unidecode.unidecode(refs.replace(",",";").replace("\n",";"))
+	
+			if "indicators" in res:
+				#print r
+				for r in p["indicators"]:
+					#print r
+					#print "**"+res["indicators"]
+					output=""
+					if "type" in r:
+						
+						if r["type"] in ["domain","hostname"]:
+							ioclist["domain"].append([r["indicator"].strip() ,name,description,author,references])
+						elif r["type"].lower().strip() == "filehash-md5":
+							ioclist["md5"].append([r["indicator"] ,name,description,author,references])
+						elif r["type"].lower().strip() == "filehash-sha256":
+							ioclist["sha256"].append([r["indicator"] ,name,description,author,references])
+						elif r["type"].lower().strip() == "ipv4":
+							ioclist["ipv4"].append([r["indicator"] ,name,description,author,references])
+						elif r["type"].lower().strip() == "url":
+							m=re.match(r"https?://([\w\d\.\-]*)[\:/]+.*",r["indicator"])
+							if not None is m and not None is m.group(1):
+								m2=re.match("\d+\.\d+\.\d+\.\d+",m.group(1))
+								if not None is m2:
+									ioclist["ipv4"].append([m.group(1) ,name,description,author,references])
+								else:
+									ioclist["domain"].append([m.group(1) ,name,description,author,references])
+	filename="ioclist.csv"
+	if len(sys.argv)>1 and len(sys.argv[1])>1:
+		filename=sys.argv[1]
+	with open(filename,"w+") as iocfile:				
+		print("IOC Type\tIndicator\tDescription")
+		iocfile.write("IOC Type,Indicator,PulseName,Description,Author,References\n")
+		lnum=0
+		for k in ioclist:
+			for e in ioclist[k]:
+				lnum+=1
+				outputline=','.join(e).strip(",")
+				outputline=k+","+outputline+"\n"
+				sys.stdout.write("\rProcessed IOC count:\t"+str(lnum))
+				iocfile.write(outputline)
+	print("\n")			
+	print("ioc list written to "+filename)
